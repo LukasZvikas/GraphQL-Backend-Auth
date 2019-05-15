c8ebd82ecb4b66c9b82619769c3ded4ee20ca3e2
LukasZvikas <lzvikas1@gmail.com>
2019-05-15 17:05:32 -0400

    fixed gitignore issues

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  MONGO_KEY_DEV: 'mongodb://LukasZvikas:Big1banger@ds151066.mlab.com:51066/graphql-dev',
-  MONGO_KEY_TEST: 'mongodb://LukasZvikas:Big1banger@ds153566.mlab.com:53566/graphql-test',
-  JWT_SECRET: 'sdfdsafsdfsdaf6764724371234712adsdasdasdasdas34324',
-  EMAIL_JWT_SECRET: '23421342314sdfasfdsfsdfadsfasdf3213123adssdas',
-  SPARKPOST_KEY: '7b2f244f8d4fbcfb2b6fbf61bb3fed377dbd04e5',
-  GMAIL_EMAIL: 'lzvikas1@gmail.com',
-  GMAIL_PASSWORD: 'Big1banger12',
-};
