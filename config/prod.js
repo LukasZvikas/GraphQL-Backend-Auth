34437fd08959aac88b3c64b0ae32f19f2ac4a0a1
LukasZvikas <lzvikas1@gmail.com>
2019-05-18 16:49:25 -0400

    Task: added key config for different environments

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MONGO_KEY_DEV: process.env.MONGO_KEY_DEV,
+  MONGO_KEY_TEST: process.env.MONGO_KEY_DEV,
+  JWT_SECRET: process.env.JWT_SECRET,
+  EMAIL_JWT_SECRET: process.env.EMAIL_JWT_SECRET,
+  GMAIL_EMAIL: process.env.GMAIL_EMAIL,
+  GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
+};
